5909cdd92fe35b8271c40391de773281dd47fbe3
ArfaMunam <arfamunam01@gmail.com>
2026-04-24 12:36:39 -0700

    Datatypes Done

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat code as new version of JS
+// alert(3 + 3);
+// alert("Hello world") this will work in browser console not here in Nodejs here there will be different syntax of alert
+// console.log("Arfa Munam");
+// console.log(123456);
+// Datatypes
+/* number string bigint null boolean(true/false) undefined symbol(primitive) object
+*/
+// Code Readability is most important in any code and it should be high. This is the first priority of coder
+let name= "Arfa Butt"
+let age = 18
+let isLoggedIn= true 
+let cityState;
+console.log(name)
+console.log(age)
+console.log(isLoggedIn)
+console.log(cityState)
+console.log(typeof name) //string
+console.log(typeof age) //number
+console.log(typeof true) //boolean
+console.log(typeof undefined) //undefined (we didn't give any value here )
+console.log(typeof null) //null is an object 
+//null is a standalone value which is representstion of emptiness

a28bcf2bc041e149a4ff8180b4bb1df2055159e4
ArfaMunam <arfamunam01@gmail.com>
2026-04-24 12:38:52 -0700

    Operations Done

---
+++ operations.js
@@ -0,0 +1,17 @@
+//Readability is very important in coding
+let value=67
+let negValue= -value
+console.log(negValue);
+console.log(23+67);
+console.log(23-7);
+console.log(23*67);
+console.log(88/6);
+console.log(23**6);
+console.log(23%6);
+console.log("3"+ 6 + 8);
+console.log(23+ 6 + "5");
+console.log("23" +8);
+let gameCounter= 400
+gameCounter++
+console.log(gameCounter);
+//Basic operations knowledge which is enough

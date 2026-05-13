57265417a92fe79270e2bfc1ff30379223d920d0
Arfa Munam Butt <arfamunam01@gmail.com>
2026-05-13 10:48:50 -0700

    Started lec

---
+++ controlFlow/one.js
@@ -0,0 +1,34 @@
+// control flow  logic flow
+// if(true){
+// console.log(`Hi there, it's Arfa Munam`);
+
+// }
+// if (false){
+//     console.log(`Not gonna executed`);
+    
+// }
+if( 2== "2"){
+    console.log(`Will be executed`);
+    
+}
+if("3" ===3){
+    console.log(`Not gonna executed`);
+    
+}
+console.log(`This is be shown as the result in the terminal`);
+
+//  == , != , === , !== , <= ,>= , < ,> 
+const isUserLoggedIn= true
+const temperature= 45
+if (temperature>50){
+    console.log("Temperature is greater than 50");
+    
+}else{
+    console.log("Temperature is less than 50");
+    
+}
+if (isUserLoggedIn== true){
+    console.log(`This statement will be printed because this is true`);
+    
+}
+console.log(`This will also be printed because JS treat it as new line of code normally`);

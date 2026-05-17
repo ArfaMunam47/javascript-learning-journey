7abec1cb900c2814d53a4fd03b9e946b3c81731d
Arfa Munam Butt <arfamunam01@gmail.com>
2026-05-17 10:50:52 -0700

    17-05-2026

--- controlFlow/switch.js
+++ controlFlow/switch.js
@@ -1,3 +1,4 @@
+// 17-05-2026
 // switch (key) {
 //     case value:
         
@@ -5,21 +6,66 @@
 
 //     default:
 //         break;
-// }
-const month= 3
+// } syntax
+const month= 8
 switch (month) {
     case 1:
-console.log("January");   
+        console.log("January");
+        console.log("This is first month");
+        
         break;
-        case 2:
-console.log("February");   
+    case 2:
+        console.log("Feburary");
+         console.log("This is second month");
         break;
-
-        case 3:
-console.log("March");   
+    case 3:
+        console.log("March");
+         console.log("This is third month");
         break;
-
+    case 4:
+        console.log("April");
+         console.log("This is fourth month");
+        break;
+    case 5:
+        console.log("May");
+         console.log("This is fifth month");
+        break;
+    case 6:
+        console.log("June");
+         console.log("This is sixth month");
+        break;
+    case 7:
+        console.log("July");
+         console.log("This is seven month");
+        break;
+    case 8:
+        console.log("August");
+         console.log("This is eighth month");
+        break;
 
     default:
+        console.log("Not mentioned here");
+        
+        break;
+}
+// ---------Checking String
+const name= "Arfa"
+switch (name) {
+    case "Ayesha":
+        console.log("I didn't mentioned this");
+        
+        break;
+    case "Arfa":
+        console.log("Information given is correct");
+        
+        break;
+    case "Aliya":
+        console.log("I didn't mentioned this");
+        
+        break;
+
+    default:
+        console.log("Checking some concept");
+        
         break;
 }
---
+++ controlFlow/truthy.js
@@ -0,0 +1,12 @@
+// Truthy and Falsy values concept
+// const email="arfamunam01@gmail.com"
+// const email="" //will consider false
+const email=[] //will consider true
+if(email){
+    console.log("Got the email");
+} else{
+    console.log("Didn't get the email");
+}
+// We suppose Values
+// ------------------------- Falsy Values
+//  0 , -0 , null , "" , undefined , NAN , bigInt

5fb45535f861f80db7075a69e25e1e623be6ccea
Arfa Munam Butt <arfamunam01@gmail.com>
2026-05-22 09:58:22 -0700

    MAP CONCEPT

--- Iterations/three.js
+++ Iterations/three.js
@@ -15,3 +15,41 @@ for (const i of name) {
     console.log(i);
     
 }
+
+// 22-05-2026 WORK
+//  MAP CONCEPT
+const map= new Map()
+// MAP is an object which contain KEY & VALUE pairs and keys should  be unique here
+// MAPS are iterable
+map.set("a",1)
+map.set("b",2)
+map.set("c",3)
+map.set("d",4)
+console.log(map.size); // will give us size of map
+console.log(map.get("a"));
+console.log(map.get("b"));
+console.log(map.get("c"));
+map.delete("d")
+console.log(map.size);
+// Maps are iterable
+// for (const key of map) {
+//     console.log(key);
+    
+// }
+// TO GET KEY AND VALUE SEPARATELY
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
+    
+}
+// OBJECTS ARE  NOT ITERABLE 
+// WE'LL CHECK IT HERE
+// let obj={
+//     name:"Khawaja Hayan",
+//     age: 18
+// }
+// for (const [key, value]  of obj) {
+//     console.log(key, ':-', value);
+    
+// } // will give us error because 'OBJECTS ARE NOT ITERABLE'
+
+

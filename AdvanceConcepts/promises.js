b738f0d9b4c38fbbe1ddcbef8be6bc1bd460f893
Arfa Munam Butt <arfamunam01@gmail.com>
2026-06-12 07:55:44 -0700

    Got stuck

--- AdvanceConcepts/promises.js
+++ AdvanceConcepts/promises.js
@@ -1,12 +1,34 @@
-// fetch('').then().catch().finally()
+// promise concept
 const promiseOne= new Promise(function(resolve,reject){
-    //asyn code
-    // db call network call cryptography
+    // asyn code JS
+    // --- db call --- network call --- cryptography
     setTimeout(function(){
-console.log('Fuck you')
+console.log('Hey there')
 resolve()
-    },2000)
+    },1000)
 })
 promiseOne.then(function(){
     console.log('Promise consumed')
 })
+//2nd method
+new Promise(function(resolve,reject){
+    // asyn code JS
+    // --- db call --- network call --- cryptography
+    setTimeout(function(){
+console.log('Hey there!! Nice to meet you')
+resolve()
+    },2000)
+}).then(function(){
+    console.log('Promise 2nd time consumed')
+})
+// 3rd time
+const promiseTwo= new Promise(function(resolve,reject){
+    setTimeout(function(){
+resolve({name:'Arfa Munam',email:'arfamunam01@gmail.com',age:20})
+    },3000)
+})
+promiseTwo.then(function(user){
+    console.log(user)
+})
+//4th method
+

6e3edad96d73a95cdc6546c4567d8de70c339cc4
Arfa Munam Butt <arfamunam01@gmail.com>
2026-06-13 12:45:22 -0700

    instanceof DONE

--- Classes & OOP/oop.js
+++ Classes & OOP/oop.js
@@ -8,9 +8,10 @@ const user={
         console.log(this)
     }
 }
+// this refers to current context
 console.log(user.getUserData())
 console.log(user.signedIn)
-console.log(this)
+console.log(this) // -----will give us empty braces
 
 // constructor function ____________________ new keyword
 // const promiseOne=new Promise()
@@ -28,6 +29,7 @@ function getData(username,isLoggedIn,age){
 // new keyword is basically used 
 const data1= new getData('Arham Farooq',true,23)
 console.log(data1.constructor) 
+console.log(data1 instanceof getData) //true
 const data2=new getData('Anashra Noor',false,24)
 console.log(data2)
 // new ___________ 
@@ -35,3 +37,11 @@ console.log(data2)
 //  _________ 2-constructor function call
 //  ______________ 3- passed arguments into this
 //  _____________ 4- give us values
+// OOP
+// OBJECT LITERAL
+// CLASSES
+// CONSTRUCTOR FUNCTION
+// PROTOTYPES
+// INSTANCES (NEW, THIS)
+
+// instanceof means _____ check whether object part of class or not and give us answer in boolean form

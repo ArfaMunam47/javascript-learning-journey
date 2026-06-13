e32213ad6c914b861ca00c2c4c0c08418c088f7a
Arfa Munam Butt <arfamunam01@gmail.com>
2026-06-13 02:33:08 -0700

    Revision Done

--- AdvanceConcepts/promises.js
+++ AdvanceConcepts/promises.js
@@ -1,34 +1,69 @@
-// promise concept
+// 13-06-2026 WORK
+// fetch('').then().catch().finally
+// PROMISE CONCEPT
+// PROMISE __________ placeholders to keep values. values to be used in futurn are kept in promises
+// THREE STATES OF PROMISE ___ pending ___ fulfilled __ rejected
+// PROMISEONE
 const promiseOne= new Promise(function(resolve,reject){
-    // asyn code JS
-    // --- db call --- network call --- cryptography
+    // JS ASYNC CODE
+    // cyrptography -- network call -- DB call
     setTimeout(function(){
-console.log('Hey there')
-resolve()
+        console.log('Arfa, the billionaire mentor')
+        resolve()
     },1000)
 })
 promiseOne.then(function(){
-    console.log('Promise consumed')
+    console.log('Hey there')
 })
-//2nd method
+// then used for RESOLVE
+
+// PROMISETWO
 new Promise(function(resolve,reject){
-    // asyn code JS
-    // --- db call --- network call --- cryptography
     setTimeout(function(){
-console.log('Hey there!! Nice to meet you')
-resolve()
+        console.log('Billionaire Mentorship Guidance')
+        resolve()
     },2000)
 }).then(function(){
-    console.log('Promise 2nd time consumed')
+    console.log('Hey there!! Whats up')
 })
-// 3rd time
-const promiseTwo= new Promise(function(resolve,reject){
+// PROMISETHREE
+const promiseThree= new Promise(function(resolve,reject){
     setTimeout(function(){
-resolve({name:'Arfa Munam',email:'arfamunam01@gmail.com',age:20})
+        console.log('Billionaire Mentorship Guidance Program')
+        resolve()
     },3000)
 })
-promiseTwo.then(function(user){
+promiseThree.then(function(){
+    console.log('Discipline your thoughts')
+})
+// PROMISEFOUR  _______________  parameters passed
+const promiseFour= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({name:'Julie Khan',gender: 'thirdGender'})
+    },4000)
+})
+promiseFour.then(function(user){
     console.log(user)
 })
-//4th method
+// PROMISEFIVE _________ we can pass multiple then() in promises 
+// catch() __________ used for holding errors
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        // let error= true
+        let error= false
+        if(!error){
+            resolve({name:'Arfa Munam', email:'arfamunam01@gmail.com'})
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+    },1000)
+})
+promiseFive.then(function(user){
+    console.log(user)
+    return user.email
+}).then(function(email){
+    console.log(email)
+}).catch(function(error){
+    console.log(error)
+})
 

e78b19e4ec382f37cfc4b90fd23bb49b18b456c7
Arfa Munam Butt <arfamunam01@gmail.com>
2026-06-13 03:13:56 -0700

    Asyn JS CODE

--- AdvanceConcepts/promises.js
+++ AdvanceConcepts/promises.js
@@ -3,7 +3,7 @@
 // PROMISE CONCEPT
 // PROMISE __________ placeholders to keep values. values to be used in futurn are kept in promises
 // THREE STATES OF PROMISE ___ pending ___ fulfilled __ rejected
-// PROMISEONE
+// PROMISE ONE
 const promiseOne= new Promise(function(resolve,reject){
     // JS ASYNC CODE
     // cyrptography -- network call -- DB call
@@ -17,7 +17,7 @@ promiseOne.then(function(){
 })
 // then used for RESOLVE
 
-// PROMISETWO
+// PROMISE TWO
 new Promise(function(resolve,reject){
     setTimeout(function(){
         console.log('Billionaire Mentorship Guidance')
@@ -26,7 +26,7 @@ new Promise(function(resolve,reject){
 }).then(function(){
     console.log('Hey there!! Whats up')
 })
-// PROMISETHREE
+// PROMISE THREE
 const promiseThree= new Promise(function(resolve,reject){
     setTimeout(function(){
         console.log('Billionaire Mentorship Guidance Program')
@@ -36,7 +36,7 @@ const promiseThree= new Promise(function(resolve,reject){
 promiseThree.then(function(){
     console.log('Discipline your thoughts')
 })
-// PROMISEFOUR  _______________  parameters passed
+// PROMISE FOUR  _______________  parameters passed
 const promiseFour= new Promise(function(resolve,reject){
     setTimeout(function(){
         resolve({name:'Julie Khan',gender: 'thirdGender'})
@@ -45,25 +45,51 @@ const promiseFour= new Promise(function(resolve,reject){
 promiseFour.then(function(user){
     console.log(user)
 })
-// PROMISEFIVE _________ we can pass multiple then() in promises 
-// catch() __________ used for holding errors
-const promiseFive = new Promise(function(resolve,reject){
+
+// PROMISE FIVE
+const promiseFive= new Promise(function(resolve,reject){
     setTimeout(function(){
-        // let error= true
-        let error= false
+        const error= false
+        // const error= true
         if(!error){
-            resolve({name:'Arfa Munam', email:'arfamunam01@gmail.com'})
+            resolve({name:'Julie Khan', gender:'thirdGender'})
         }else{
-            reject('ERROR: Something went wrong')
+            reject('ERROR : Something went wrong')
         }
-    },1000)
+    },5000)
 })
 promiseFive.then(function(user){
-    console.log(user)
-    return user.email
-}).then(function(email){
-    console.log(email)
-}).catch(function(error){
+console.log(user)
+return user.gender
+})
+.then(function(gender){   // make code more readable and prettier
+    console.log(gender)
+})
+.catch(function(error){
     console.log(error)
 })
+// we can use multiple then in our code / promises
 
+// ANOTHER  METHOD (_______________ try catch()________________ )
+// this is used for gracefully handling of things
+// PROMISE SIX
+const promiseSix= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        const error= false
+        // const error= true
+        if(!error){
+            resolve({name:'JS', password : '@123'})
+        }else{
+            reject('ERROR : JS went wrong')
+        }
+    },6000)
+})
+async function consumePromiseSix(){
+   try {
+    const response=  await promiseSix
+   console.log(response)
+   } catch (error) {
+    console.log(error)
+   }
+}
+consumePromiseSix()

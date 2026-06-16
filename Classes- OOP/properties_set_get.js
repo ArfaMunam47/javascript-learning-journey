1fca166e49874edb37ab3602a3d39d76c83d235c
Arfa Munam Butt <arfamunam01@gmail.com>
2026-06-16 03:09:13 -0700

    Revision Done

--- Classes- OOP/properties_set_get.js
+++ Classes- OOP/properties_set_get.js
@@ -1,26 +1,26 @@
-function user(username,password){
-    // this.password=password
-    //_______________ username
+function user(username,password,email){
+    this.email=email
+    // function is both object and function
     Object.defineProperty(this,'username',{
         get:function(){
             return this._username.toUpperCase()
         },
         set:function(name){
-this._username=username
-        }
+            this._username=name
+        },
     })
-    //____________________ password
     Object.defineProperty(this,'password',{
         get:function(){
             return `${this._password}abc`
         },
         set:function(value){
-this._password=value
-        }
+            this._password=value
+        },
     })
-     this.username=username
-     this.password=password
+       this.username=username
+       this.password=password
 }
-const user1=new user('Bisma','345')
+const user1= new user('Farooq','@123','farooq009@gmail.com')
 console.log(user1.username)
 console.log(user1.password)
+// GETTER AND SETTER THROUGH PROPERTIES
--- Classes- OOP/setter and getter.js
+++ Classes- OOP/setter and getter.js
@@ -1,26 +1,24 @@
-// SETTER AND GETTER FUNCTION 
+// 16-06-2026 REVISION
+// SETTER AND GETTER FUNCTION
 class user{
-constructor(username,email,password){
-    this.username=username
-    this.email=email
-    this.password=password
+    constructor(username,email,password){
+        this.username=username
+        this.email=email
+        this.password=password
+    }
+    get username(){
+        return this._username.toUpperCase()
+    }
+    set username(name){
+        this._username=name
+    }
+    get password(){
+        return `${this._password}#23`
+    }
+    set password(value){
+        this._password=value
+    }
 }
-//_________________ username
-get username(){
-    return this._username.toUpperCase()
-}
-set username(name){
-    this._username=name
-}
-//________________________ password
-get password(){
-    // return this._password.toUpperCase()
-    return `${this._password}@#`
-}
-set password(value){
-    this._password=value
-}
-}
-const user1= new user('Arfa','arfamunam01@gmail.com','abc')
+const user1= new user('Arfa','arfamunam01@gmail.com','@123')
 console.log(user1.username)
 console.log(user1.password)

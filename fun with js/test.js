416fa60ca9631888ab8602f3adfb964f3e6b4fdd
Arfa Munam Butt <arfamunam01@gmail.com>
2026-06-17 11:13:56 -0700

    Basic git Command

---
+++ fun with js/test.js
@@ -0,0 +1,22 @@
+const myArr=[]
+%DebugPrint(myArr)
+
+// ________________ in GIT BASH
+// pwd
+// ls 
+// ls -la
+
+// node -v
+// npm -v
+// npm install jsvu(JAVASCRIPT VERSION UPDATER) -g
+// jsvu
+// v8-debug (most important)
+// ~/.jsvu/bin/engines
+// ~/.jsvu/bin/v8-debug
+//v8-debug -v
+// which v8-debug
+// cmd (move to directory)
+// cmd oneDrive cmd Desktop cmd JS
+// cd run command automatically
+
+// d8> print('Hello')
